04851513412ad999a5d2c143230ef6e5e9637559
Wesley_Assis <88063740+WesleyAssis1999@users.noreply.github.com>
2022-11-16 01:13:50 -0300

    Create servidor.py

---
+++ servidor.py
@@ -0,0 +1,18 @@
+import socket
+
+PORTA = 15000
+
+servidor = socket.socket()
+servidor.bind(("localhost", PORTA))
+servidor.listen(5)
+print("Servidor escutando na porta: ", PORTA)
+cliente, endereco = servidor.accept()
+print("Conexao aceita de: ", endereco)
+
+while True:		
+	mensagem = cliente.recv(1024)
+	print("Mensagem recebida: ", mensagem.decode())
+
+	mensagem = input("Digite uma mensagem: ")
+	cliente.send(mensagem.encode())	
+	#cliente.close()
