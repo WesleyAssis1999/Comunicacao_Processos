b196d8e02b51261ba095fed70606ba61fa92c3e4
Wesley Assis <88063740+WesleyAssiss@users.noreply.github.com>
2022-11-21 21:32:09 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,7 +1,7 @@
 import socket
 
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cliente.connect(("localhost", 80100))
+cliente.connect(("localhost", 14600))
 
 while True:    
     mensagem = input("Digite uma mensagem: ")
